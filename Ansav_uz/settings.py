753f0b533ccb538b71ba6993cb032068313fd336
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2021-10-23 16:17:15 +0000

    settings;

--- Ansav_uz/settings.py
+++ Ansav_uz/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-cz8my_wd=r*y!v!*6!n_nncc%8xmq70r23!qs+y+j4y=y)=x@$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['EC2.DNS.Ansav.uz', '127.0.0.1']
+ALLOWED_HOSTS = ['EC2.DNS.Ansav.uz', '127.0.0.1', 'localhost']
 
 
 # Application definition
