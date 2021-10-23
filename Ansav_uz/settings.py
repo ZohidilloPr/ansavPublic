2c3fec3ee26dd43d8a71c36e158381b4ebad2e7f
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2021-10-23 21:30:20 +0500

    sql

--- Ansav_uz/settings.py
+++ Ansav_uz/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ansavdb',
-        'USER': 'admin',
+        'USER': 'postgres',
         'PASSWORD': 'adminansav2021',
         'HOST': 'localhost',
         'PORT': '',
